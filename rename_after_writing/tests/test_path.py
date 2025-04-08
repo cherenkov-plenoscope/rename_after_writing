a5adfd828c4ce9106705b4a5c7277ee59b555408
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2025-04-08 13:27:26 +0200

    test do not alter input path string.

--- rename_after_writing/tests/test_path.py
+++ rename_after_writing/tests/test_path.py
@@ -1,6 +1,8 @@
 import rename_after_writing as rnw
 import tempfile
+import copy
 import os
+import glob
 
 
 def test_contextmanager():
@@ -20,3 +22,23 @@ def test_contextmanager():
                 back = foo.read()
 
             assert back == "omg\n"
+
+
+def test_do_not_alter_input_path():
+    for use_tmp_dir in [False, True, None]:
+        with tempfile.TemporaryDirectory() as tmp:
+
+            original_path = os.path.join(tmp, "123.txt")
+            original_path_copy = copy.copy(original_path)
+
+            with rnw.Path(
+                path=original_path, use_tmp_dir=use_tmp_dir
+            ) as rnw_path:
+                with open(rnw_path, "wt") as foo:
+                    foo.write("omg\n")
+
+            num_files_in_outdir = len(glob.glob(os.path.join(tmp, "*")))
+            assert num_files_in_outdir == 1
+
+            assert os.path.exists(original_path_copy)
+            assert original_path == original_path_copy
