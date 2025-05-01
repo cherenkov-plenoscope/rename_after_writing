ac11838a4cdf2293ab33ec189165a576d76c656d
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2025-05-01 13:00:31 +0200

    test directory

---
+++ rename_after_writing/tests/test_directory.py
@@ -0,0 +1,27 @@
+import rename_after_writing as rnw
+import tempfile
+import copy
+import os
+import glob
+
+
+def test_contextmanager():
+    for use_tmp_dir in [False, True, None]:
+        with tempfile.TemporaryDirectory() as tmp:
+            dir_path = os.path.join(tmp, "123.dir")
+            assert not os.path.exists(dir_path)
+            filename = "456.txt"
+
+            with rnw.Directory(
+                path=dir_path, use_tmp_dir=use_tmp_dir
+            ) as rnw_dir:
+                with open(os.path.join(rnw_dir, filename), "wt") as foo:
+                    foo.write("omg\n")
+
+            assert not os.path.exists(rnw_dir)
+            assert os.path.exists(dir_path)
+
+            with open(os.path.join(dir_path, filename), "rt") as foo:
+                back = foo.read()
+
+            assert back == "omg\n"
